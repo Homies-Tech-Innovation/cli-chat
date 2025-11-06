ae0ef9c5f905421ef2b6bb03681c2584474478ac
Shrisha <126317780+desstroyy@users.noreply.github.com>
2025-11-06 12:36:13 +0530

    First step - Creating a server to listen to requests (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::net::{TcpStream, TcpListener};
+
+fn create_server() -> () {
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:5000").expect("Problems in binding with port");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_stream(&stream);
+            },
+            Err(e) => {
+                println!("{}",e);
+            }
+        }
+    }
+    ()
+}
+
+fn handle_stream(_stream: &TcpStream) -> () {
+    println!("Stream connected");
+    ()
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    create_server();
 }
