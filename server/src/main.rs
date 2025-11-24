2f4a9e49fd144e6a46142cb668c089624e95dd33
SSK <shrishask884@gmail.com>
2025-11-24 14:03:57 +0530

    basic comms done

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,32 +1,33 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Write;
 use std::thread;
 use std::io::Read;
+use std::sync::Arc;
+use std::sync::mpsc;
 
 fn create_server() -> () {
-    let listener = TcpListener::bind("127.0.0.1:5000").expect("Failure to bind to port: ");
+    let  listener = TcpListener::bind("127.0.0.1:5000").expect("Failure to bind to port: ");
+    let mut senders: Vec<mpsc::Sender::<String>> = Vec::new();
+
     for stream in listener.incoming() {
+        let (tx1, rx1) = mpsc::channel(); //ser to cli
+        let (tx2, rx2) = mpsc::channel(); //cli to ser
+        senders.push(tx1);
         let stream = stream.expect("Error in unwrapping the result");
-        thread::spawn(|| {
-        handle_client(stream);
+        thread::spawn(move || {
+            handle_client(stream, rx1, tx2);
         });
+        let msg = rx2.recv().unwrap();
+        broadcast_message(msg, &senders);
     }
     ()
 }
 
-fn handle_client(mut socket: TcpStream) -> () {
+fn handle_client(mut socket: TcpStream, rx: mpsc::Receiver::<String>, tx: mpsc::Sender::<String>) -> () {
     println!("Connection done");
     let mut s: String = String::new();
-
-    let _ = socket.read_to_string(&mut s).expect("Error in reading to string");
-    println!("Received message");
-    broadcast_message(s, &mut socket);
-
-    ()
-}
-
-fn broadcast_message(message: String, mut socket: &TcpStream) -> (){
-    socket.write_all(&message.as_bytes()).expect("Error in broadcasting message");
+    socket.read_to_string(&mut s).expect("Error in reading to string");
+    tx.send(s).unwrap();
+    () } fn broadcast_message(message: String, _senders: &Vec<mpsc::Sender<String>>) -> () { println!("{}",message);
     ()
 }
 
