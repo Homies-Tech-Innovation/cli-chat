0fd7e14c12ae0f5d1cab57999fb759d97717cb1e
kasimlyee <kasiimlyee@gmail.com>
2026-01-18 18:51:19 +0300

    The server all clients and individually

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,33 +1,94 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Write;
+use std::io::{Write, BufReader, BufRead};
+use std::sync::{Arc, Mutex};
 use std::thread;
-use std::io::Read;
+
+type ClientList = Arc<Mutex<Vec<TcpStream>>>;
+
 
 fn create_server() -> () {
-    let listener = TcpListener::bind("127.0.0.1:5000").expect("Failure to bind to port: ");
+    let listener = TcpListener::bind("127.0.0.1:5000").expect("Failed to bind to port: ");
+    
+    let clients: ClientList = Arc::new(Mutex::new(Vec::new()));
+
+    println!("Server listening on port 5000\n");
+
     for stream in listener.incoming() {
-        let stream = stream.expect("Error in unwrapping the result");
-        thread::spawn(|| {
-        handle_client(stream);
-        });
+        match stream{
+            Ok(stream) => {
+                let clients = Arc::clone(&clients);
+                thread::spawn(move || {
+                    handle_client(stream, clients);
+                });
+            }
+            Err(e) =>{
+                eprint!("Error Accepting connection: {}", e);
+            }
+        }
     }
     ()
 }
 
-fn handle_client(mut socket: TcpStream) -> () {
-    println!("Connection done");
-    let mut s: String = String::new();
+fn handle_client(socket: TcpStream, clients: ClientList) -> () {
+    let addr = socket.peer_addr().ok();
+    println!("New connection: {:?}", addr);
 
-    let _ = socket.read_to_string(&mut s).expect("Error in reading to string");
-    println!("Received message");
-    broadcast_message(s, &mut socket);
+    //We clone the stream for broacasting
+    let socket_clone = socket.try_clone().expect("Failed to colne socket");
 
-    ()
+    //Then we add client to the client list
+    {
+        let mut client_list = clients.lock().unwrap();
+        client_list.push(socket_clone);
+        println!("Total clients: {}\n", client_list.len());
+    }
+    let mut reader = BufReader::new(&socket);
+    let mut line = String::new();
+
+    loop{
+        line.clear();
+        match reader.read_line(&mut line){
+            Ok(0) => {
+                println!("Client disconnected: {:?}", addr);
+                break;
+            }
+            Ok(_) =>{
+                let message = line.trim();
+                if !message.is_empty(){
+                    println!("Received message from {:?}: {}", addr, message);
+                    broadcast_message(message.to_string(), &clients, &socket);
+                }
+            }
+            Err(e) =>{
+                eprintln!("Error reading from client {:?}: {}", addr, e);
+                break;
+            }
+        }
+    }
+    
 }
 
-fn broadcast_message(message: String, mut socket: &TcpStream) -> (){
-    socket.write_all(&message.as_bytes()).expect("Error in broadcasting message");
-    ()
+fn broadcast_message(message: String, clients: &ClientList,sender: &TcpStream) -> (){
+    let sender_addr = sender.peer_addr().ok();
+    let broadcast = format!("Broadcast: {}\n", message);
+
+    let mut client_list = clients.lock().unwrap();
+    let mut disconnected = Vec::new();
+
+    for (i, client) in client_list.iter_mut().enumerate()
+{
+    if let Ok(addr)= client.peer_addr()
+{
+    if Some(addr) != sender_addr{
+        if let Err(e) = client.write_all(broadcast.as_bytes()){
+            eprintln!("Error sending message to {:?}: {}", addr, e);
+            disconnected.push(i);
+        }
+    }
+}}    for &i in disconnected.iter().rev(){
+    client_list.remove(i);
+}
+println!("Active clients: {}", client_list.len());
 }
 
 
